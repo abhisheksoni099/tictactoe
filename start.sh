9873c99e1b7e68a2081449728f4110f0fe416607
Abhishek Soni <abhisheksoni099@gmail.com>
2023-08-31 15:56:01 +0530

    Add readme, Update start script, Change artifact name

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-java -cp target/PROJECT-tictactoe-1.0-jar-with-dependencies.jar com.abhisheksoni.tictactoe.Runner StartGame 2 u1 x u2 0 3
+mvn clean compile assembly:single
+java -cp target/tictactoe-1.0-jar-with-dependencies.jar com.abhisheksoni.tictactoe.Runner StartGame 2 u1 x u2 0 3
